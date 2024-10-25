cef33bfd76107b44b4bf97e4b634aa7a15e78f81
juaperez <juaperez@uade.edu.ar>
2024-10-25 17:35:39 -0300

    fix cors v1

--- backend/src/main/java/com/integracion/backend/config/WebConfig.java
+++ backend/src/main/java/com/integracion/backend/config/WebConfig.java
@@ -2,12 +2,15 @@ package com.integracion.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
+    /*
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -20,5 +23,20 @@ public class WebConfig {
                         .allowCredentials(true);
             }
         };
+    }
+    */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*"); // Usa allowedOriginPattern en lugar de allowedOrigins
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
