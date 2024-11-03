d2da8f11da6ce9d4931f38b275ff771d1284ae26
juaperez <juaperez@uade.edu.ar>
2024-11-03 03:52:24 -0300

    env update

--- backend/src/main/java/com/integracion/backend/BackendApplication.java
+++ backend/src/main/java/com/integracion/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.integracion.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
